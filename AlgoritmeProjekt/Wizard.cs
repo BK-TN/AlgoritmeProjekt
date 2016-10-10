4ca35f592d4da2f70f9a0b252855ac28b0e7dd1d
Sameh Cheheiber <evilfridge@live.dk>
2016-10-10 11:10:35 +0200

    Entity class inheritance

---
+++ AlgoritmeProjekt/Wizard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+namespace AlgoritmeProjekt
+{
+    class Wizard : Entity
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
