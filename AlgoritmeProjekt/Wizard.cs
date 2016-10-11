50e1343c5f9a0f6a32d8e4904da97c4000fbb555
thomas munk <thomas.munk@gmail.com>
2016-10-11 10:37:29 +0200

    wizard is moving !!!!

--- AlgoritmeProjekt/Wizard.cs
+++ AlgoritmeProjekt/Wizard.cs
@@ -6,59 +6,88 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
 using System.Timers;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AlgoritmeProjekt
 {
     internal class Wizard : Entity
     {
-<<<<<<< HEAD
-        int i;
-        
-        List<Point> path;
-        public Wizard(int x, int y) : base(x, y)
+        float X;
+        float Y;
+        int CurrentPath;
+        private Texture2D sprite;
+        List<Vector2> path;
+        public Wizard() : base()
         {
-            i = -1;
-            path = new List<Point>();
+            CurrentPath =0;
+
+            path = new List<Vector2>();
+            path.Add(new Vector2(150, 10));
+            path.Add(new Vector2(100, 100));
+            path.Add(new Vector2(250, 100));
+            path.Add(new Vector2(10, 100));
+            path.Add(new Vector2(10, 10));
+            
         }
 
-=======
->>>>>>> 680b494cc1d040603a3eeeb3eca55397b14e2dc0
+
         public override void LoadContent(ContentManager contentManager)
         {
-            //TODO: Load wizard sprite
+            sprite = contentManager.Load<Texture2D>("wizard");
         }
 
-        public void WizardPath(List<Point> list)
+        public void WizardPath(List<Vector2> list)
         {
+            CurrentPath = 0;
             path = list;
+
+        }
+        public void goToPoint(Vector2 tal, float deltaTime)
+        {
+            
+            if (tal.X < X)
+            {
+                X-= 35*deltaTime;
+                this.Position = new Vector2(X, Y);
+            }
+            if (tal.X > X)
+            {
+                X += 35*deltaTime;
+                this.Position = new Vector2(X, Y);
+            }
+            if (tal.Y < Y)
+            {
+                Y-= 35*deltaTime ;
+                this.Position = new Vector2(X, Y);
+            }
+            if (tal.Y > Y)
+            {
+                Y += 35*deltaTime;
+                
+                this.Position = new Vector2(X, Y);
+            }
+            
+
+            if (Vector2.Distance(tal, new Vector2(X,Y)) <= 1 && CurrentPath != path.Count - 1)
+            {
+            this.Position = new Vector2(tal.X, tal.Y);
+            CurrentPath++;
+            }
+               
 
         }
 
         public override void Update(float deltaTime)
         {
             base.Update(deltaTime);
-
-            foreach( Point tal in path)
-            {
-                while(tal.X < this.X)
-                {
-                    this.X--;
-                }
-                while(tal.X > this.X)
-                {
-                    this.X++;
-                }
-                while (tal.Y < this.Y)
-                {
-                    this.Y--;
-                }
-                while (tal.Y > this.Y)
-                {
-                    this.Y++;
-                }
-
-            }
-            
+        
+            goToPoint(path.ElementAt(CurrentPath), deltaTime);
+                       
+            this.Position = new Vector2(X,Y);       
+        }
+        public override void Draw(SpriteBatch target)
+        {
+            target.Draw(sprite, new Vector2(Position.X, Position.Y));
         }
 
     }
