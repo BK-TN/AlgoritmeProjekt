81af2740be2d224288a796d69530039d64fa1fef
BK-TN <me@bk-tn.com>
2016-10-11 10:37:55 +0200

    Monster now (hopefully) gets solid when a wizard steps on it

--- AlgoritmeProjekt/Monster.cs
+++ AlgoritmeProjekt/Monster.cs
@@ -13,5 +13,23 @@ namespace AlgoritmeProjekt
         {
             //TODO: Load monster sprite
         }
+
+        public override void Update(float deltaTime)
+        {
+            foreach (Wizard wiz in World.Entities.OfType<Wizard>())
+            {
+                int wizX = ((int)(wiz.Position.X / 48)) * 48;
+                int wizY = ((int)(wiz.Position.Y / 48)) * 48;
+
+                int myX = ((int)(Position.X / 48)) * 48;
+                int myY = ((int)(Position.Y / 48)) * 48;
+
+                if (wizX == myX && wizY == myY)
+                {
+                    //Wizard has now stepped on monster
+                    Solid = true;
+                }
+            }
+        }
     }
 }
