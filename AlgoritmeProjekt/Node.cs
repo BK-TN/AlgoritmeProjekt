a38e90e664d634c4a1eda032ad0f72fb38f6a36e
Sameh Cheheiber <evilfridge@live.dk>
2016-10-11 11:44:01 +0200

    Node shit

---
+++ AlgoritmeProjekt/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace AlgoritmeProjekt
+{
+    enum GridType { Sart, Goal, Solid, Empty}
+
+   class Node
+    {
+        private int g = 0;
+        private int h = 0;
+        private int f;
+
+        private Node parent;
+
+        //grid position
+        private Point pos;
+
+        public Node(Point pos)
+        {
+            this.pos = pos;
+        }
+
+        public Node Parent { get; set; }
+        public int G { get; set; }
+        
+    }
+}
