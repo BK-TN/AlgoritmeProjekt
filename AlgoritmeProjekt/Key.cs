8641dc0912f5dc0616963d448ffa0264d16a1e76
Sameh Cheheiber <evilfridge@live.dk>
2016-10-11 10:29:32 +0200

    draw shit on map

--- AlgoritmeProjekt/Key.cs
+++ AlgoritmeProjekt/Key.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AlgoritmeProjekt
 {
     internal class Key : Entity
     {
+        private Texture2D sprite;
+
+        public Key() { }
         public override void LoadContent(ContentManager contentManager)
         {
             //TODO: Load key sprite
+            sprite = contentManager.Load<Texture2D>("key");
+        }
+
+        public override void Draw(SpriteBatch target)
+        {
+            target.Draw(sprite, new Vector2(Position.X, Position.Y));
         }
     }
 }
